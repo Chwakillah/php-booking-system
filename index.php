6f7b8898fbf22c3ae189f379d004e9049299b53a
Chwakillah <syakillahnachwa081@gmail.com>
2024-01-21 09:40:04 +0700

    menambahkan folder database

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    $my_name = "Syakillah Nachwa";
+    $my_name = "CUY RESTO GACOR";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $my_name?></title>
+</head>
+<body>
+    
+</body>
+</html>
